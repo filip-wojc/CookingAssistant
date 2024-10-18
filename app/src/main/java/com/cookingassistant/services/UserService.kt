73414f76a62086812f3e883af4f6d393f1b01ffa
DarknessoPirate <dp307910@student.polsl.pl>
2024-10-18 14:22:52 +0200

    added register function to UserService

--- app/src/main/java/com/cookingassistant/services/UserService.kt
+++ app/src/main/java/com/cookingassistant/services/UserService.kt
@@ -2,14 +2,20 @@ package com.cookingassistant.services
 
 import com.cookingassistant.data.DTO.LoginRequest
 import com.cookingassistant.data.DTO.LoginResponse
+import com.cookingassistant.data.DTO.RegisterRequest
+import com.cookingassistant.data.TokenRepository
 import com.cookingassistant.data.network.ApiRepository
 import retrofit2.Response
 
-class UserService(private val api: ApiRepository) {
+class UserService(private val apiRepository: ApiRepository) {
     suspend fun logInUser(email: String, password:String): Response<LoginResponse> {
-        val request = LoginRequest(email = email, password = password)
-        return api.logIn(request)
+        val requestBody = LoginRequest(email = email, password = password)
+        return apiRepository.logIn(requestBody)
     }
     // TODO: finish registration
-    // suspend fun registerUser(username: String, email: String, password: String): Response<>
+    suspend fun registerUser(username: String, email: String, password: String): Response<String?>
+    {
+        val requestBody = RegisterRequest(username, email, password)
+        return apiRepository.register(requestBody)
+    }
 }
