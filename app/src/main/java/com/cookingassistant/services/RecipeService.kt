dc9dd7e4adee850ca7a043c312a32fc25a8684c4
DarknessoPirate <dp307910@student.polsl.pl>
2024-10-19 02:23:04 +0200

    added getAllNutrientsList function to fetch the list of ingredients in db

--- app/src/main/java/com/cookingassistant/services/RecipeService.kt
+++ app/src/main/java/com/cookingassistant/services/RecipeService.kt
@@ -1,8 +1,11 @@
 package com.cookingassistant.services
 
 import com.cookingassistant.data.network.ApiRepository
+import retrofit2.Response
 
 class RecipeService(private val apiRepository: ApiRepository) {
-
+    suspend fun getAllNutrientsList(): Response<List<String>> {
+        return apiRepository.getAllNutrientsList()
+    }
 
 }
