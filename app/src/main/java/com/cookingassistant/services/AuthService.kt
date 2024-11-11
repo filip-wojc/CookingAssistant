24acd5aed4a58dd0527545a5482e96d0d6241d69
DarknessoPirate <dp307910@student.polsl.pl>
2024-11-11 19:41:39 +0100

    modified AuthService functions to fit new Response handling

--- app/src/main/java/com/cookingassistant/services/AuthService.kt
+++ app/src/main/java/com/cookingassistant/services/AuthService.kt
@@ -2,21 +2,39 @@ package com.cookingassistant.services
 
 import com.cookingassistant.data.DTO.LoginRequest
 import com.cookingassistant.data.DTO.LoginResponse
+import com.cookingassistant.data.Models.Result
 import com.cookingassistant.data.DTO.RegisterRequest
 import com.cookingassistant.data.repositories.ApiRepository
+import com.cookingassistant.util.ApiResponseParser
 import retrofit2.Response
 
 class AuthService(private val _apiRepository: ApiRepository) {
+    private val _apiResponseParser = ApiResponseParser()
 
-    suspend fun logInUser(email: String, password:String): Response<LoginResponse> {
-        val requestBody = LoginRequest(email = email, password = password)
-        return _apiRepository.logIn(requestBody)
+    // TODO: ADD EXCEPTION HANDLING
+    suspend fun logInUser(email: String, password:String): Result<LoginResponse?> {
+        val requestBody = LoginRequest(
+            email,
+            password
+        )
+        val response = _apiRepository.logIn(requestBody)
+        val result = _apiResponseParser.wrapResponse(response)
+
+        return result
     }
 
-    suspend fun registerUser(username: String, email: String, password: String): Response<String?>
+    // TODO: ADD EXCEPTION HANDLING
+    suspend fun registerUser(username: String, email: String, password: String): Result<Unit?>
     {
-        val requestBody = RegisterRequest(username, email, password)
-        return _apiRepository.register(requestBody)
+        val requestBody = RegisterRequest(
+            username,
+            email,
+            password
+        )
+        val response = _apiRepository.register(requestBody)
+        val result = _apiResponseParser.wrapResponse(response)
+
+        return result
     }
 
 
